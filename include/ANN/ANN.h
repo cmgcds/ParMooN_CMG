dfffd7ad16e1fdeefde6bb84ba72b84ea5bba513
Subodh Joshi <smjoshi07@gmail.com>
2020-12-24 16:13:41 +0530

    New module on Artificial Neural Networks added. Changes include:
    1. New folder 'ANN' created in include and src
    2. A new class TANN declared in include/ANN/ANN.h and defined in
    src/ANN/ANN.C
    3. CMakeLists.txt file added in src/ANN to compile the ANN relevant
    files and create a static library 'ann'
    4. CMakeLists.txt file of the main code in the root dir. modified to
    include the ANN Folders as well as the static library ann
    
    This new class 'TANN' would contain the neural network, read and write
    functionality as well as  metadata. This class in turn invokes functions
    from MLPACK. All the MLPACK includes are ONLY declared in ANN.h and
    nowhere else in ParMooN code to keep it clean.
    
    To use this class, just include TANN.h in ParMooN code and start using
    the class as well as other MLPACK features.

---
+++ include/ANN/ANN.h
@@ -0,0 +1,30 @@
+// =======================================================================
+// @(#)TANN.h        1.0 24.12.20
+// 
+// Class:       TANN
+// Purpose:     Class for Artificial Neural Networks (ANN)
+//
+// Author:      Subodh M. Joshi (24.12.20)
+//
+// History:     start of implementation 24.12.20 (Subodh M. Joshi)
+//
+// =======================================================================
+
+#ifndef __TANN__
+#define __TANN__
+
+#include <mlpack/core.hpp>
+
+/** Class for artificial neural networks (ANN) **/
+class TANN
+{
+  public:
+  
+  /** constructor */  
+  TANN();
+  /** destrcutor */
+  ~TANN();
+
+};
+
+#endif
---
+++ src/ANN/ANN.C
@@ -0,0 +1,14 @@
+/** ************************************************************************ 
+* @brief     source file for TANN
+* @author    Subodh M. Joshi
+* @date      24.12.20
+* @History   Implementation started 24.12.20 (Subodh M. Joshi) 
+ ************************************************************************  */
+
+#include <ANN.h>
+
+TANN::TANN(){};
+
+TANN::~TANN(){};
+
+

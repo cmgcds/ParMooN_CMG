8e794a349c2db40792ff44018e38a056ea5d3514
Subodh Joshi <smjoshi07@gmail.com>
2021-03-11 09:31:07 +0530

    Forward run scripts for sensitivity analysis added.

---
+++ ANNSensitivityAnalysis/Run2HL.py
@@ -0,0 +1,104 @@
+import numpy as np
+import os
+from dataFunctions import *
+
+def runSimulations(thisRunDir):
+# Activation function array for output layer
+# Note: 0: sigmoid, 1:identity, 2:leakyReLU, 3:tanH
+    OPLTYPE_ARRAY = [0,1,3,4];
+
+# Dimension array for the hidden layer
+    HL_DIM_ARRAY = [5,7,10];
+
+# Activation function array for the hidden layer
+# Note: 0: sigmoid, 1:identity, 2:ReLU, 3:leakyReLU, 4:tanH
+    HL_TYPE_ARRAY = [ 0,3,4];
+
+# Epochs array
+    EPOCHS_ARRAY = [10,100];
+
+# Number of hidden layers fixed at 2 for this file
+    NHL = 2;
+
+
+# Initialization a dictionary of all the parameters
+    data = dict();
+    data["NHL"] = NHL;
+    data["OPLTYPE"] = 1;
+    data["HL_0_DIM"] = 5;
+    data["HL_0_TYPE"] = 3;
+    data["HL_1_DIM"] = 5;
+    data["HL_1_TYPE"] = 3;
+    data["IPDATADIM"] = 1;
+    data["IPLDIM"] = 3;
+    data["OPLDIM"] = 1;
+    data["DATASET_NAME"] = "trainingData.csv";
+    data["TRAINING_DATA_PERCENTAGE"] = 73;
+    data["VALIDATION_DATA_PERCENTAGE"] = 7;
+    data["OPTIMIZER_CODE"] = 3;
+    data["OPTIMIZER_STEP_SIZE"] = 0.001;
+    data["SGD_BATCH_SIZE"] = 32;
+    data["MAX_ITERATIONS"] = 1000000;
+    data["TOLERANCE"] = 1e-8;
+    data["DROPOUT_RATIO"] = 0.2;
+    data["EPOCHS"] = 100;
+    data["SAVE_DATA_FILE"] = "testResults.csv";
+
+
+
+# Start the loops
+
+    listDir = os.listdir(thisRunDir);
+    maxDir = 0;
+    if (listDir == ['metadata.dat', 'inputSpace.dat']):
+        maxDir = 0;
+    else:
+        listDir.remove('metadata.dat');
+        listDir.remove('inputSpace.dat');
+        listDir.sort(key=lambda f: int(''.join(filter(str.isdigit, f))))
+        maxDir = int(listDir[-1]);
+        maxDir = maxDir + 1;
+
+    count = maxDir;
+    for OPLTYPE in OPLTYPE_ARRAY:
+        for HL_0_DIM in HL_DIM_ARRAY:
+            for HL_0_TYPE in HL_TYPE_ARRAY:
+                for HL_1_DIM in HL_DIM_ARRAY:
+                    for HL_1_TYPE in HL_TYPE_ARRAY:
+                        for EPOCHS in EPOCHS_ARRAY:
+                            # Create a directory to save the configuration and the result
+                            
+                            os.mkdir(thisRunDir+'/' +str(count));
+
+                            # Update the dictionary for this run
+                            data["OPLTYPE"] = OPLTYPE;
+                            data["HL_0_DIM"] = HL_0_DIM;
+                            data["HL_0_TYPE"] = HL_0_TYPE;
+                            data["HL_1_DIM"] = HL_1_DIM;
+                            data["HL_1_TYPE"] = HL_1_TYPE;
+                            data["EPOCHS"] = EPOCHS;
+                            data["SAVE_DATA_FILE"] = "testResults.csv";
+
+                            # Create the code script with this data 
+                            createScript(data);
+
+                            # Send a copy of this script to the respective results directory
+                            os.system("cp runScript.dat "+thisRunDir+'/'+str(count)+'/');
+
+                            # Write the input parameter space specifications in inputSapceFile
+                            inputSpaceFile = open(thisRunDir+'/inputSpace.dat', 'a');
+                            inputSpaceFile.write(str(count)+'   '+  str(NHL)+'   '+ str(OPLTYPE)+ '   ' + str(HL_0_DIM) + '   ' + str(HL_0_TYPE) +  '   ' + str(HL_1_DIM) + '   ' + str(HL_1_TYPE) + '   ' + str(0) + '   '+ str(1) + '   '+  str(EPOCHS) + '   \n');
+
+                            inputSpaceFile.close();
+                            # Run the simulation
+                            os.system('./parmoon_2D_SEQUENTIAL.exe runScript.dat');
+
+                            # Copy the results file to the respective folder
+                            os.system("mv ' testResults.csv' "+thisRunDir+'/'+str(count)+'/testResults.csv');
+
+                            print(count);
+
+                            # Update the file
+                            count += 1;
+
+
---
+++ ANNSensitivityAnalysis/RunExpt.py
@@ -0,0 +1,84 @@
+import os
+from datetime import datetime
+from decimal import Decimal
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+from dataFunctions import *
+
+import Run1HL as H1
+import Run2HL as H2
+import Run3HL as H3
+
+projectName = "ANN";
+
+#______________________________________
+# Create paths and manage directories
+#______________________________________
+# Output directory
+outputPath = os.getcwd()+'/output';
+
+# Location for storing test case. Change 'ANN' to another name for different project
+projectOutputDir = outputPath+'/'+projectName; 
+
+# If the project of the given project-name doesn't exist, then create it
+if not os.path.exists(projectOutputDir):
+    os.makedirs(projectOutputDir)
+
+# Location of the current run of the project (note: there could be multiple runs for the same project) created on different occasions
+# Each run in-turn will have multiple folders each corresponding to a sample point from input space
+# If no runs exist so far, a folder '1' will be automatically created for a run
+# Else a new folder will be created having number as 'last maximum number + 1'
+listDir = os.listdir(projectOutputDir);
+if (listDir == []):
+    thisRun = '0';
+else:
+    listDir.sort(key=lambda f: int(''.join(filter(str.isdigit, f))))
+    lastRun = int(listDir[-1]);
+    thisRun = str(lastRun + 1);
+
+thisRunDir = projectOutputDir+'/'+thisRun;
+# Create a folder for this run
+os.mkdir(thisRunDir);
+
+#_______________________________________________________
+# Create Metadata file
+#_______________________________________________________
+startDateTime = datetime.now() 
+date = startDateTime.strftime("%d-%b-%Y")
+time = startDateTime.strftime("%H:%M:%S")
+
+#_______________________________________________________
+# Start writing metadata in a file inside the current project run
+#_______________________________________________________
+metadataFile = open(thisRunDir+'/metadata.dat', 'a');
+metadataFile.write('Case name = '+ projectName);
+metadataFile.write('\nTest run number = '+ thisRun);
+metadataFile.write('\nAddress = '+ thisRunDir);
+metadataFile.write('\nDate = '+ date);
+metadataFile.write('\nStarting time (h:m:s) = '+ time);
+
+
+#_______________________________________________________
+# Start writing input space in a file inside the current project run
+#_______________________________________________________
+inputSpaceFile = open(thisRunDir+'/inputSpace.dat', 'a');
+inputSpaceFile.write('#sampleNo. NHL OPLTYPE HL_0_DIM HL_0_TYPE HL_1_DIM HL_1_TYPE HL_2_DIM HL_2_TYPE EPOCHS\n');
+
+inputSpaceFile.close();
+
+H1.runSimulations(thisRunDir);
+H2.runSimulations(thisRunDir);
+H3.runSimulations(thisRunDir);
+
+
+
+#_______________________________________________________
+# Finish writing metadata 
+#_______________________________________________________
+endDateTime = datetime.now() 
+time = endDateTime.strftime("%H:%M:%S")
+metadataFile.write('\nFinal time (h:m:s) = ' + time); 
+metadataFile.close();
+

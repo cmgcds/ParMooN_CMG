6fbbd03deee5ed9582cf9b6ef35466d2905413b4
thivinthegreat <thivinthegreat@gmail.com>
2023-05-17 12:28:28 +0530

    Added ABL models

--- Main_Users/Thivin/Examples/NSE2D/ABL.h
+++ Main_Users/Thivin/Examples/NSE2D/ABL.h
@@ -77,19 +77,33 @@ void BoundCondition(int i, double t, BoundCond &cond)
 void U1BoundValue(int BdComp, double Param, double &value)
 {
        // INLET = 0 // OUTLET = 1 // walls = 2 // cylinder = 3
-    if (Param > 0 ) cout << "Param Exists" << endl;
+    // if (Param > 0 ) cout << "Param Exists" << endl;
   switch(BdComp)
   {
-    case 0: value = 1 ; //value = -(Param)*(Param-1)*4 ;
+    case 0: 
+            // Convert the param value, which is fom 0 to 1 to actual x value which is from 0 to 500
+            value = 0;
             break;
     case 1: value = 0;
             break;
     case 2: value =0;
             break;
-    case 3: value = 0;
-            break;
-    case 4: value = 1;
-            break;  
+    case 3: 
+        double actual_y = Param * 500;
+        
+        double uStar = 0.3;
+        double kappa = 0.41;
+        double z0 = 4;
+        // Compute natural log
+        // q: syntax for natural log in C++
+        // a: 
+
+        double u = (uStar/kappa) * log( (actual_y + z0)/z0);
+        cout <<  " Param : " << Param << " ;; actual_y: " << actual_y << " ;; u : " << u << endl;
+        value = u;
+        break;  
+    
+       
     default: cout << "wrong boundary part number" << endl;
   }
 }
@@ -98,7 +112,7 @@ void U2BoundValue(int BdComp, double Param, double &value)
 {
  switch(BdComp)
   {
-    case 0: value = 1 ; //value = -(Param)*(Param-1)*4 ;
+    case 0: value = 0 ; //value = -(Param)*(Param-1)*4 ;
             break;
     case 1: value = 0;
             break;
@@ -106,7 +120,7 @@ void U2BoundValue(int BdComp, double Param, double &value)
             break;
     case 3: value = 0;
             break;
-    case 4: value = 1;
+    case 4: value = 0;
             break;  
     default: cout << "wrong boundary part number" << endl;
   }
